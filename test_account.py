01b50005ed7463f7ffa993c59f7dda17347df483
vjacintoflores <violetflores788@gmail.com>
2023-04-21 21:05:45 -0500

    added comments

--- test_account.py
+++ test_account.py
@@ -12,30 +12,36 @@ class Test:
         assert self.a1.get_name() == 'John'
 
     def test_deposit(self):
-        # negative, zero, positive
+        # negative
         assert self.a1.deposit(-2.55) is False
         assert self.a1.get_balance() == 0
 
+        # zero
         assert self.a1.deposit(0) is False
         assert self.a1.get_balance() == 0
 
+        # positive
         assert self.a1.deposit(2.55) is True
         assert self.a1.get_balance() == 2.55
 
     def test_withdraw(self):
-        # negative, zero, positive invalid, positive valid
+        # negative
         assert self.a1.withdraw(-2.5) is False
         assert self.a1.get_balance() == 0
 
+        # zero
         assert self.a1.withdraw(0) is False
         assert self.a1.get_balance() ==0
 
+        # positive invalid
         assert self.a1.withdraw(20) is False
         assert self.a1.get_balance() == 0
 
+        # deposit amount
         assert self.a1.deposit(100) is True
         assert self.a1.get_balance() == 100
 
+        # positive valid
         assert self.a1.withdraw(20) is True
         assert self.a1.get_balance() == 80
 
