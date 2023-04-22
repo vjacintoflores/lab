bf0f4b636e33d82bd44e4300efc709fb812fba57
vjacintoflores <violetflores788@gmail.com>
2023-04-21 20:41:09 -0500

    added second file

---
+++ test_account.py
@@ -0,0 +1,38 @@
+import pytest
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+    def test_init(self):
+        assert self.a1.get_name() == 'John'
+    def test_deposit(self):
+        # negative, zero, positive
+        assert self.a1.deposit(-2.55) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(2.55) is True
+        assert self.a1.get_balance() == 2.55
+    def test_withdraw(self):
+        # negative, zero, positive invalid, positive valid
+        assert self.a1.withdraw(-2.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() ==0
+
+        assert self.a1.withdraw(20) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(100) is True
+        assert self.a1.get_balance() == 100
+
+        assert self.a1.withdraw(20) is True
+        assert self.a1.get_balance() == 80
+
